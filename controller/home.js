89e7bd1afa36f32e703beedaa28ac49017aedd6d
Ben T <ben.trevethan@gmail.com>
2021-04-07 16:31:18 +0930

    removed handlebars because: reasons

--- controller/home.js
+++ controller/home.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const path = require('path')
 
 router.get('/', async (req, res) => {
     try {
-        res.render('home')
+        res.sendFile(path.join(__dirname, '../public/html/index.html'))
     } 
     catch (err) {
         res.status(500).json(err)
